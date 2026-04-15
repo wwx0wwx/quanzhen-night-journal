b9746d3846f85398f5f71330d27a1eaa8fd7b766
wwx0wwx <3180102784@zju.edu.cn>
2026-04-15 08:12:28 +0800

    preserve runtime caddy config on restart

--- caddy/entrypoint.sh
+++ caddy/entrypoint.sh
@@ -4,6 +4,8 @@ set -eu
 
 mkdir -p /runtime
 
-cp /etc/caddy/Caddyfile /runtime/Caddyfile
+if [ ! -s /runtime/Caddyfile ]; then
+  cp /etc/caddy/Caddyfile /runtime/Caddyfile
+fi
 
 exec caddy run --config /runtime/Caddyfile --adapter caddyfile
